9b3a90fed7355fa92d1d733be801b38035621ec7
SibiyonS <sibiyon2004@gmail.com>
2024-11-28 18:41:44 +0530

    Update settings.py

--- internship_experience/settings.py
+++ internship_experience/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i7r)f2!b-)_sir@hvev8%m^=*%9yjjy&no@dw30zbl=i6g&+si'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','127.0.0.1']
 
