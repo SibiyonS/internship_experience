8a5085301cd94c0c70d18db1caa60db1b9edd5f8
SibiyonS <sibiyon2004@gmail.com>
2024-11-28 13:24:47 +0530

    Update urls.py

--- internship/urls.py
+++ internship/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from .views import download_database
 from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
     path('register/', views.register, name='register'),
     path('success/', views.success, name='success'),
+    path('download-db/', download_database, name='download_database'),
 ]
