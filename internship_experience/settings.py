e70e048aef2ca1e61a181cd43daf3b460b95c97b
SibiyonS <sibiyon2004@gmail.com>
2024-11-28 17:40:59 +0530

    Update settings.py

--- internship_experience/settings.py
+++ internship_experience/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/internship/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
