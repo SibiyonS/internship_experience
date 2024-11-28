1f6ae25420d40b8143edc46ea6df9139aaa9a9cd
SibiyonS <sibiyon2004@gmail.com>
2024-11-28 16:50:36 +0530

    Update urls.py

--- internship/urls.py
+++ internship/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('register/', views.register, name='register'),
     path('success/', views.success, name='success'),
-    path('email-db/', email_database, name='email_database'),
 ]
